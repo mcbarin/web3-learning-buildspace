25a5979ebd3c5a1889c14dbf8b3aa490e70198a5
Cagatay Barin <cagatay@crowddatasystems.com>
2021-12-11 23:51:47 +0300

    End of project

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -4,11 +4,16 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-// WavePortal address: 0x831F6aC8f19940B8a6E3E831198680Ff128aa00A
 
 contract WavePortal {
     uint256 totalWaves;
     uint256 totalPokes;
+
+    /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
+
 
     // Events
     event NewWave(address indexed from, uint256 timestamp, string message);
@@ -30,15 +35,63 @@ contract WavePortal {
     Wave[] waves;
     Poke[] pokes;
 
-    constructor() {
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastInteractionPerAddress;
+
+    constructor() payable {
         console.log("Yo yo, I am a contract and I am smart (I guess)");
+
+        /*
+         * Set the initial seed
+         */
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(string memory _message) public {
+
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastInteractionPerAddress[msg.sender] + 1 minutes < block.timestamp,
+            "Van minut"
+        );
+
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastInteractionPerAddress[msg.sender] = block.timestamp;
+
         totalWaves += 1;
         console.log("%s waved w/ message %s", msg.sender, _message);
 
         waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        
+        console.log("Random # generated: %d", seed);
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "I'm broke"
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+
 
         emit NewWave(msg.sender, block.timestamp, _message);
     }
@@ -48,6 +101,30 @@ contract WavePortal {
         console.log("%s poked w/ message %s", msg.sender, _message);
 
         pokes.push(Poke(msg.sender, _message, block.timestamp));
+
+        /*
+         * Generate a new seed for the next user that sends a wave
+         */
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        
+        console.log("Block Difficulty: %d", block.difficulty);
+        console.log("Block Timestamp: %d", block.timestamp);
+        console.log("Random # generated: %d", seed);
+
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (seed <= 50) {
+            console.log("%s won!", msg.sender);
+
+            uint256 prizeAmount = 0.00001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "I'm broke"
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
 
         emit NewPoke(msg.sender, block.timestamp, _message);
     }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,12 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: '[ALCHEMY_URL]',
-      accounts: '[ACCOUNT_ADDRESS]',
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
